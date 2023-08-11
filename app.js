249c75bc2150108944b6ad4e4a11254da426ceec
ElFlaco08 <welin1924@gmail.com>
2023-08-11 16:52:26 +0200

    Variables Var,Let,Const En JavaScript

--- app.js
+++ app.js
@@ -35,7 +35,23 @@
 //     "professional": true
 // }
 
-console.log('Esto Es una cadena')
-console.log(15)
-console.log(true)
-console.log({username: 'Welington', score: 80.4, hours: 16, professional: true})
+// console.log('Esto Es una cadena')
+// console.log(15)
+// console.log(true)
+// console.log({username: 'Welington', score: 80.4, hours: 16, professional: true})
+
+
+// Variables
+
+var nombreDelUsuario = 'Welington '; //Var
+
+let apellido = 'Santos'; //let 
+nombreDelUsuario = 'Welington Manuel ';
+apellido = 'Villar Santos ';
+
+
+const edad = 15;  //Const - Constante
+
+
+
+console.log(nombreDelUsuario + apellido + "Edad " + edad)
