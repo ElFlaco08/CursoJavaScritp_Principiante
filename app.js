45d416f8c4097fe83d04fed581fba52bc05d9b7f
ElFlaco08 <welin1924@gmail.com>
2023-08-11 20:04:26 +0200

    Bucles en JavaScript :)

--- app.js
+++ app.js
@@ -152,9 +152,17 @@ switch(typeCard) {
 
 // Bucles 
 
-let count = 50;
+let count = 10;
 
 while (count > 0) {
     console.log(count);
     count = count - 1
+}
+
+
+let names = ['Welington', 'villar', 'Santos'];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+    
 }
