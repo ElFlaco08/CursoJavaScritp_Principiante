e31f9ec6c55c0b008b9d1508dbd99ecd21375a83
ElFlaco08 <welin1924@gmail.com>
2023-08-11 16:20:48 +0200

    Tipos De Datos En JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 document.write("<h1>Hello World</h1>")
+
+//Tipos De Datos
+
+"Hello World"   // String
+'Hello World'  // String
+
+10000000  //Number
+1000.2    //Number
+5         //Number
+-3        //Number
+4         //Number
+2         //Number
+
+
+true       //Boolean
+false      //Boolean
